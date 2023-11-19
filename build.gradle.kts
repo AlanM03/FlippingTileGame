c222fa6490b29eb6c03b3fdc569919cd143ac711
ddamme05 <ddamme05@gmail.com>
2023-11-19 17:22:37 -0500

    Added JavaFX functionality to Gradle project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.openjfx.javafxplugin") version "0.0.10"
 }
 
 group = "org.fliptile"
@@ -13,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // Add your JavaFX dependencies here if needed
 }
 
 tasks.test {
@@ -23,8 +25,13 @@ application {
     mainClass.set("org.fliptile.Main")
 }
 
-tasks.named<JavaExec>("run") {
-    standardInput = System.`in`
+javafx {
+    version = "17.0.9" // Specify the JavaFX version
+    modules = listOf("javafx.controls", "javafx.fxml") // List the JavaFX modules you're using
+    // Add other modules as required
 }
 
-
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+    // If you need to pass VM options for JavaFX, you can do so here, although the plugin should handle it
+}
