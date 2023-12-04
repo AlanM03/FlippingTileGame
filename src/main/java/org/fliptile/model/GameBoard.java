00dabd2d804dd2c504eb7e8408a127ec8b7094bc
ddamme05 <ddamme05@gmail.com>
2023-12-04 00:21:08 -0500

    Added JavaDoc for GameBoard class.

--- src/main/java/org/fliptile/model/GameBoard.java
+++ src/main/java/org/fliptile/model/GameBoard.java
@@ -4,13 +4,23 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents the game board in a memory game, consisting of a grid of tiles.
+ */
 public class GameBoard {
     private final Tile[][] board;
     private final int rows;
     private final int columns;
     private int remainingPairs;
 
-
+    /**
+     * Constructs a GameBoard with specified dimensions and a list of image identifiers.
+     *
+     * @param rows The number of rows in the game board.
+     * @param columns The number of columns in the game board.
+     * @param imageIdentifiers A list of image identifiers for creating tile pairs.
+     * @throws IllegalArgumentException if the total number of tiles (rows * columns) is not even.
+     */
     public GameBoard(int rows, int columns, List<String> imageIdentifiers) {
         if (rows * columns % 2 != 0) {
             throw new IllegalArgumentException("The number of tiles must be even.");
@@ -23,6 +33,11 @@ public class GameBoard {
         initializeBoard(imageIdentifiers);
     }
 
+    /**
+     * Initializes the board with shuffled tiles based on the provided image identifiers.
+     *
+     * @param imageIdentifiers A list of image identifiers for creating tile pairs.
+     */
     private void initializeBoard(List<String> imageIdentifiers) {
         List<Tile> tiles = new ArrayList<>();
         for (String identifier : imageIdentifiers) {
@@ -41,6 +56,13 @@ public class GameBoard {
         }
     }
 
+    /**
+     * Checks if two tiles are a match.
+     *
+     * @param firstTile The first tile to be checked.
+     * @param secondTile The second tile to be checked.
+     * @return True if the tiles match, false otherwise.
+     */
     public boolean checkMatch(Tile firstTile, Tile secondTile) {
         if (firstTile.matches(secondTile)) {
             remainingPairs--;
@@ -49,22 +71,47 @@ public class GameBoard {
         return false;
     }
 
+    /**
+     * Returns the number of rows in the game board.
+     *
+     * @return The number of rows.
+     */
     public int getRows() {
         return rows;
     }
 
+    /**
+     * Returns the number of columns in the game board.
+     *
+     * @return The number of columns.
+     */
     public int getColumns() {
         return columns;
     }
 
+    /**
+     * Returns the game board as a 2D array of Tiles.
+     *
+     * @return The 2D array of Tiles.
+     */
     public Tile[][] getBoard() {
         return board;
     }
 
+    /**
+     * Returns the number of remaining pairs that need to be matched.
+     *
+     * @return The number of remaining pairs.
+     */
     public int getRemainingPairs() {
         return remainingPairs;
     }
 
+    /**
+     * Returns the total number of pairs on the game board.
+     *
+     * @return The total number of pairs.
+     */
     public int getTotalPairs() {
         return (rows * columns) / 2;
     }
