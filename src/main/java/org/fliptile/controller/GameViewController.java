6bfe65b5bef1c66589a9b32d3e7bf3277dc18bb6
ddamme05 <ddamme05@gmail.com>
2023-12-03 21:42:36 -0500

    Tile images now show up on JavaFX Application Window.

--- src/main/java/org/fliptile/controller/GameViewController.java
+++ src/main/java/org/fliptile/controller/GameViewController.java
@@ -1,14 +1,18 @@
 package org.fliptile.controller;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.fliptile.model.GameManager;
+import org.fliptile.model.Tile;
 
 import java.io.IOException;
 
@@ -48,14 +52,52 @@ public class GameViewController {
     }
 
     public void onTileClick(int row, int col) {
-        if (firstTileRow == null || firstTileColumn == null) {
-            firstTileRow = row;
-            firstTileColumn = col;
-        } else {
+        Tile clickedTile = gameManager.getGameBoard().getBoard()[row][col];
+        Button tileButton = (Button) tileGrid.getChildren().get(row * gridSize + col);
+
+        // Flip the tile
+        clickedTile.flip();
+
+        // Update tile image
+        updateTileImage(clickedTile, tileButton);
+
+        // If it was the second tile clicked, process the move
+        if (firstTileRow != null && firstTileColumn != null && !(firstTileRow == row && firstTileColumn == col)) {
             gameManager.processMove(firstTileRow, firstTileColumn, row, col);
             updateUI();
-            firstTileRow = null; // Reset for the next turn
+            // Flip the tiles back if they are not a match after a short delay
+            if (!clickedTile.matches(gameManager.getGameBoard().getBoard()[firstTileRow][firstTileColumn])) {
+                final int savedFirstTileRow = firstTileRow;
+                final int savedFirstTileColumn = firstTileColumn;
+
+                PauseTransition pause = new PauseTransition(Duration.seconds(1));
+                pause.setOnFinished(event -> {
+                    // Use savedFirstTileRow and savedFirstTileColumn here
+                    Tile firstTile = gameManager.getGameBoard().getBoard()[savedFirstTileRow][savedFirstTileColumn];
+                    Button firstButton = (Button) tileGrid.getChildren().get(savedFirstTileRow * gridSize + savedFirstTileColumn);
+                    firstTile.flip();
+                    clickedTile.flip();
+                    updateTileImage(firstTile, firstButton);
+                    updateTileImage(clickedTile, tileButton);
+                });
+                pause.play();
+            }
+            firstTileRow = null;
             firstTileColumn = null;
+        } else {
+            firstTileRow = row;
+            firstTileColumn = col;
+        }
+    }
+
+    private void updateTileImage(Tile tile, Button tileButton) {
+        if (tile.isFlipped()) {
+            ImageView tileImage = new ImageView(tile.getImage());
+            tileImage.setFitWidth(50);
+            tileImage.setFitHeight(50);
+            tileButton.setGraphic(tileImage);
+        } else {
+            tileButton.setGraphic(null);
         }
     }
 
