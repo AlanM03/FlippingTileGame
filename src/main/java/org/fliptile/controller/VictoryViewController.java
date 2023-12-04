bf08a4b6235efbbde5916b344dd7daf6b6e2f179
ddamme05 <ddamme05@gmail.com>
2023-12-04 00:28:31 -0500

    Added JavaDoc for VictoryViewController class.

--- src/main/java/org/fliptile/controller/VictoryViewController.java
+++ src/main/java/org/fliptile/controller/VictoryViewController.java
@@ -3,14 +3,27 @@ package org.fliptile.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * Controller class for the victory screen in the memory game.
+ */
 public class VictoryViewController {
     @FXML private Label scoreLabel;
     @FXML private Label moveCountLabel;
 
+    /**
+     * Sets the score label with the provided score.
+     *
+     * @param score The final score achieved by the player.
+     */
     public void setScoreLabel(int score) {
         scoreLabel.setText("Score: " + score);
     }
 
+    /**
+     * Sets the move count label with the provided move count.
+     *
+     * @param moveCount The number of moves taken by the player.
+     */
     public void setMoveCountLabel(int moveCount) {
         moveCountLabel.setText("Moves: " + moveCount);
     }
