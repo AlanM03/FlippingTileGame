64c5bdd6d7f8d0329276b30c40bd90f96d2e0f76
ddamme05 <ddamme05@gmail.com>
2023-12-03 19:19:15 -0500

    GameView works with AnchorPane, reset game with score does not.

--- src/main/java/org/fliptile/view/Main.java
+++ src/main/java/org/fliptile/view/Main.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GameView.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("GameView.fxml")));
         primaryStage.setTitle("Flipping Tile Game");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
